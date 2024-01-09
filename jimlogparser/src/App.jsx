6cc100f1940e2b48f2f399ca423ff894e53ba7dc
PeterDavids <peter@rawstadia.com>
2024-01-09 15:28:13 +0100

    format

--- jimlogparser/src/App.jsx
+++ jimlogparser/src/App.jsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react'
 import { useCallback } from 'react'
 import ReactJson from 'react-json-view'
 
-class LineCountError extends Error {}
+class LineCountError extends Error { }
 
 function App() {
 	const [fileContent, setFileContent] = useState('')
@@ -12,35 +12,36 @@ function App() {
 	const [depth, setDepth] = useState(2)
 
 	const handleFileChange = (event) => {
-	  const file = event.target.files[0]
-	  if (file) {
-		const reader = new FileReader()
-		reader.onload = (e) => {
-		  const content = e.target.result
-		  setFileContent(content)
+		const file = event.target.files[0]
+		if (file) {
+			const reader = new FileReader()
+			reader.onload = (e) => {
+				const content = e.target.result
+				setFileContent(content)
+			}
+			reader.readAsText(file)
 		}
-		reader.readAsText(file)
-	  }
 	}
 
-	const assertParseResult = useCallback(( linesArr, output) => {
-		const entryCount = linesArr.filter(l => l.slice(0,4) === new Date().getFullYear() + '').length
-		if (entryCount !== output.length){
-			throw new LineCountError('LineCountError', entryCount, ' => ' , output.length)
+	const assertParseResult = useCallback((linesArr, output) => {
+		const entryCount = linesArr.filter(l => l.slice(0, 4) === new Date().getFullYear() + '').length
+		if (entryCount !== output.length) {
+			throw new LineCountError('LineCountError', entryCount, ' => ', output.length)
 		}
-	},[])
+	}, [])
 
 	const parseTextLogWithConsole = useCallback(() => {
-		if (!fileContent) return 
+		if (!fileContent) return
 		const output = []
 		const lines = fileContent.split('\r\n')
-		
+
 		for (const l of lines) {
-			if (l.slice(0,4) === new Date().getFullYear() + '') {
+			if (l === '') continue
+			if (l.slice(0, 4) === new Date().getFullYear() + '') {
 				if (l.includes('{')) {
 					const presumedJsonStart = l.indexOf('{')
 					output.push({
-						"action": l.slice(0,presumedJsonStart),
+						"action": l.slice(0, presumedJsonStart),
 						"json": JSON.parse(l.slice(presumedJsonStart)),
 					})
 					continue
@@ -49,14 +50,14 @@ function App() {
 			} else {
 				const presumedJsonStart = l.indexOf('{')
 				try {
-					output[output.length -1] = {
-						"action": output[output.length -1],
+					output[output.length - 1] = {
+						"action": output[output.length - 1],
 						"json": JSON.parse(l.slice(presumedJsonStart)),
 					}
 				} catch (e) {
-					console.log('ERROR:', e)
+					console.log('ERROR at line:', l)
+					console.log(e)
 				}
-
 			}
 		}
 
@@ -69,40 +70,43 @@ function App() {
 			setParsedContent(output)
 		}
 
-	},[fileContent])
+	}, [fileContent])
 
 	useEffect(() => {
 		parseTextLogWithConsole()
-	},[fileContent])
-  
+	}, [fileContent])
+
 	return (
-	  <div style={{width: '90vw'}}>
-		<input
-		  type="file"
-		  onChange={handleFileChange}
-		  accept=".txt"
-		/>
-		<div>
-		  
-		  {!fileContent ? <h2>Upload PlainText Log File</h2> : 
-		  <div>
-		  	Formatted output in console
-			<div>
-				Collapsed after depth
-				<input style={{marginLeft: '1em', width: '3em'}} type="number" step='1' value={depth} onChange={(e) => {
-					const val = Number(e.target.value)
-					if (Number.isFinite(val)) 
-						setDepth(val)
-					}}/>
-			</div>
-			<ReactJson src={parsedContent} theme="rjv-default" collapsed={depth}
-			displayDataTypes={false}
-			displayObjectSize={true}
+		<div style={{ width: '90vw' }}>
+			<input
+				type="file"
+				onChange={handleFileChange}
+				accept=".txt"
 			/>
-		  </div>
-		  }
+			<div>
+
+				{!fileContent ? <h2>Upload PlainText Log File</h2> :
+					<div>
+						Formatted output in console
+						<div>
+							Collapsed after depth
+							<input style={{ marginLeft: '1em', width: '3em' }} type="number" step='1' value={depth} onChange={(e) => {
+								const val = Number(e.target.value)
+								if (Number.isFinite(val))
+									setDepth(val)
+							}} />
+						</div>
+						<ReactJson
+							src={parsedContent}
+							theme="rjv-default"
+							collapsed={depth}
+							displayDataTypes={false}
+							displayObjectSize={true}
+						/>
+					</div>
+				}
+			</div>
 		</div>
-	  </div>
 	);
 }
 
